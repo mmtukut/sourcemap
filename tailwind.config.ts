f8b0ff188fa358735815d9531947a2c20af67af8
Muhd Muhd Tukur <muhammadmuhammadtukur13@gmail.com>
2025-11-03 06:21:36 +0000

    revert to the correct files structure and route. fix this and any error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,9 @@ import type {Config} from 'tailwindcss';
 export default {
   darkMode: ['class'],
   content: [
-    './src/**/*.{ts,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     container: {
