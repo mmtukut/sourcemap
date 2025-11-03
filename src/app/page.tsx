c03d7efb40364801062bdf8766b89131b3002866
Muhd Muhd Tukur <muhammadmuhammadtukur13@gmail.com>
2025-11-03 07:26:58 +0000

    fonts and route fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 import { redirect } from 'next/navigation';
 
-export default function AppRootPage() {
-  redirect('/dashboard');
 export default function RootPage() {
   redirect('/landing');
 }
- 
